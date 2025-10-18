1ef7acea0397c1264b4fff23280f6a7888b568e2
Shpro123456789 <107074467+Shoro123456789@users.noreply.github.com>
2025-10-18 12:58:36 +0100

    removed comments of prev unused code

--- question2_optimisation_attempt.py
+++ question2_optimisation_attempt.py
@@ -15,8 +15,6 @@ def paths(sample):
     smallest_path_word=0
     for i in range(0,len(sample)-1):
         smallest_path_word += l(sample[i],sample[i+1])
-#    print(smallest_path_word)
-#    print(largest_path_word)
     return smallest_path_word
 def permutations(word: str,smallest: int, largest: int):
     for i in itertools.permutations(word):
@@ -35,23 +33,11 @@ permutation= itertools.permutations(word)
 combos = []
 for val in permutation:
     combos.append(str(''.join(val)))
-#print(combos)
-#print(len(combos))
-#paths("ritangle")
 smallest=paths("ritangle")
 small_word=""
 largest=paths("ritangle")
 large_word=""
 smallest, small_word, largest, large_word = permutations("ritangle",smallest,largest)
-#for i in combos:
-#    contender=paths(i)
-#    if int(contender)<smallest:
-#        smallest=contender
-#        small_word=i
-#    if contender>largest:
-#        largest=contender
-#        large_word=i
-#        print(i)
 end=time.time()
 print("Time taken: "+str(end-start)+" seconds")
 print("smallest: "+small_word+",  "+str(smallest))
