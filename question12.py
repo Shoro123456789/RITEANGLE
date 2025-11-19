4df2d42e63c46029d729880b7851ffeb4eb25238
Shpro123456789 <107074467+Shoro123456789@users.noreply.github.com>
2025-11-19 18:46:28 +0000

    solved q12

---
+++ question12.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy import stats
+counter=0
+for HH in range(0,100):
+    for MM in range(0,60):
+        for SS in range(0,60):
+            sample=np.array([],dtype=int)
+            for i in list(str("{0:0=2d}".format(HH))):
+                sample=np.append(sample,int(i))
+            for i in list(str("{0:0=2d}".format(MM))):
+                sample=np.append(sample,int(i))
+            for i in list(str("{0:0=2d}".format(SS))):
+                sample=np.append(sample,int(i))
+            #stat= stats.mode(sample)
+            #print(str(stat.count)+","+str(stat.mode))
+            #if stat.count==2:
+            #    counter+=1
+            #    print(str(HH)+":"+str(MM)+":"+str(SS)+"     count:"+str(counter))
+            if len(np.unique(sample))==2:
+                counter+=1
+                print(sample+"     count:"+str(counter))
+print(counter)
